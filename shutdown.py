033511833cd78e4675ec779b5a006898a3a9ed57
Jose Trejo <joseatrejos@gmail.com>
2021-03-20 03:48:38 -0700

    First push

---
+++ shutdown.py
@@ -0,0 +1,19 @@
+import time, os
+
+timeout = 60*50   # 50 minutes in seconds
+timeout_start = time.time()
+shutdown = False
+
+while time.time() < timeout_start + timeout:
+    if(time.time() + 5 >= timeout_start + timeout):
+        shutdown = True
+        print("Now going to shut down")
+
+    # Print the remaining time to shut down
+    print("I'm still " + str( (timeout_start + timeout) - (time.time() + 5) ) +
+    " seconds away from shutting down.")
+    # Wait for 10 seconds
+    time.sleep(10)
+    
+if(shutdown == true):
+    os.system('shutdown -s')
